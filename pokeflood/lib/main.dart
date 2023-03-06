1ee0ff742dec2f56baeb5ce86a89273928e6c503
Icare741 <94448198+Icare741@users.noreply.github.com>
2023-03-06 10:50:48 +0100

    ⚡️add .env for api request

--- pokeflood/lib/main.dart
+++ pokeflood/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pokeflood/pokemonHome.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'pokemonHome.dart';
 
-void main() {
+void main()async {
+  await dotenv.load(fileName: "lib/.env");  //path to your .env file);
   runApp(const MyApp());
 }
 
--- pokeflood/lib/pokemonHome.dart
+++ pokeflood/lib/pokemonHome.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'pokemon.dart';
-
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class PokemonListWidget extends StatefulWidget {
   const PokemonListWidget({required Key key}) : super(key: key);
@@ -31,24 +30,22 @@ class _PokemonListWidgetState extends State<PokemonListWidget> {
   }
 
   Future<void> _fetchPokemonList() async {
-    final response = await http.get(Uri.parse('https://mon-api-pokemon.vercel.app/api/pokemon'));
+    final response = await http.get(Uri.parse('${dotenv.env['API_BASE_URL']}pokemon?limit=151'));
     final json = jsonDecode(response.body);
-    print(json);
     final results = json['results'];
 
     List<Pokemon> pokemonList = [];
     for (var i = 0; i < results.length; i++) {
       final pokemon = results[i];
-      final name = pokemon['name']['fr'];
-      final imageUrl = pokemon['sprites']['regular'];
-      final types = List<String>.from(pokemon['types']['name']);
+      final name = pokemon['name'];
+      final imageUrl = '${dotenv.env['POKEMON_SPRITE_BASE_URL']}${i + 1}.png';
+      final typesUrl = await http.get(Uri.parse(pokemon['url']));
+      final typesJson = jsonDecode(typesUrl.body);
+      final types = List<String>.from(typesJson['types'].map((type) => type['type']['name']));
 
       pokemonList.add(Pokemon(name, imageUrl, types));
-      print(imageUrl);
-      print(name);
-      print(types);
     }
-    
+
     setState(() {
       _pokemonList = pokemonList;
       _searchResults = pokemonList;
